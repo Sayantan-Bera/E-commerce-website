c595f3b5616461c951bf2f7cc7968e85b9d8b2bf
Sahil-Ahmed19 <ahmedsahilcr7@gmail.com>
2023-04-04 01:19:39 +0530

    added customer model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+STATE_CHOICES = (
+    ('Andaman & Nicobar Islands','Andaman & Nicobar Islands'),
+    ('Andhra Pradesh','Andhra Pradesh'),
+    ('Arunachal Pradesh','Arunachal Pradesh'),
+    ('Assam','Assam'),
+    ('Bihar','Bihar'),
+    ('Chandigarh','Chandigarh'),
+    ('Chhattisgarh','Chhattisgarh'),
+    ('Dadra & Nagar Haveli','Dadra & Nagar Haveli'),
+    ('Daman and Diu','Daman and Diu'),
+    ('Delhi','Delhi'),
+    ('Goa','Goa'),
+    ('Gujarat','Gujarat'),
+    ('Haryana','Haryana'),
+    ('Tamil Nadu','Tamil Nadu'),
+    ('Telangana','Telangana'),
+    ('Tripura','Tripura'),
+    ('Uttarakhand','Uttarakhand'),
+    ('West Bengal','West Bengal'),
+)
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharFiels(max_length=50)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices= STATE_CHOICES, max_length=50)
+
+def __str__(self):
+    return str(self.id)
+
+    
 
 # Create your models here.
