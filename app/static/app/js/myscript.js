34f9ab35b970ea3daf951ec6e5e6cd12ca553e36
Priyankar Sarkar <priyankarsarkar2020@gmail.com>
2023-04-10 00:37:48 +0530

    navbar style updated

--- app/static/app/js/myscript.js
+++ app/static/app/js/myscript.js
@@ -25,15 +25,48 @@ $('#slider1, #slider2, #slider3').owlCarousel({
 const menu_button = document.querySelector(".menu-button");
 const menu_close_button = document.querySelector(".menu-close-button");
 const toggle_menu = document.querySelector(".toggle-menu");
+const dropdown_head = document.querySelectorAll(".dropdown-head");
+const dropdowns = document.querySelectorAll(".drop-menu");
+
+dropdown_head.forEach((item)=>{
+    const dropdown = item.parentElement.lastElementChild;
+
+    item.addEventListener("click",()=>{
+        if(dropdown.classList.contains("show-drop-menu"))
+            dropdown.classList.remove("show-drop-menu");
+        else
+            dropdown.classList.add("show-drop-menu");
+    });
+
+    item.addEventListener("mouseover",()=>{
+        if(window.innerWidth < 850) return;
+        dropdown.classList.add("show-drop-menu");
+
+        item.addEventListener("mouseout",()=>{
+            if(window.innerWidth < 850) return;
+            dropdown.classList.remove("show-drop-menu");
+        });
+    });
+    
+    dropdown.addEventListener("mouseover",()=>{
+        if(window.innerWidth < 850) return;
+        dropdown.classList.add("show-drop-menu");
+    });
+
+    dropdown.addEventListener("mouseout",()=>{
+        if(window.innerWidth < 850) return;
+        dropdown.classList.remove("show-drop-menu");
+    });
+})
 
 menu_button.addEventListener("click", ()=>{
-    toggle_menu.style.visibility="visible";
+    toggle_menu.classList.add("show-toggle-menu");
     menu_button.style.visibility="hidden";
     menu_close_button.style.visibility="visible";
 })
 
 menu_close_button.addEventListener("click", ()=>{
-    toggle_menu.style.visibility="hidden";
+    toggle_menu.classList.remove("show-toggle-menu");
     menu_close_button.style.visibility="hidden";
     menu_button.style.visibility="visible";
 })
