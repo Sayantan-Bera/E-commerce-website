1a9ccd9139215ac3238e136fd77ba297fbe5da1f
Priyankar Sarkar <priyankarsarkar2020@gmail.com>
2023-05-12 02:18:18 +0530

    Order page done

--- app/forms.py
+++ app/forms.py
@@ -46,6 +46,6 @@ class MyPasswordResetForm(PasswordResetForm):
     )
 class MySetPasswordForm(SetPasswordForm):
     new_password1=forms.CharField(label=_("New Password"),
-    strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'new-password','class':'form-control'}),
+    strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'new-password','class':'each-form-input-inner'}),
     help_text=password_validation.password_validators_help_text_html())
-    new_password2=forms.CharField(label=("Confirm New Password"),strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'new-password','class':'form-control'}))
+    new_password2=forms.CharField(label=("Confirm New Password"),strip=False,widget=forms.PasswordInput(attrs={'autocomplete':'new-password','class':'each-form-input-inner'}))
